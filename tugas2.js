fe10a8e0a3922655c5243254679784830c6da306
Muhammad Nur Faza <gofar222@gmail.com>
2023-05-13 10:55:01 +0700

    added changes

--- tugas2.js
+++ tugas2.js
@@ -3,3 +3,9 @@ var string = "string";
 var boolean = true;
 var nul = null;
 var arr = [2, 3, 4, 5];
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(nul);
+console.log(arr);
